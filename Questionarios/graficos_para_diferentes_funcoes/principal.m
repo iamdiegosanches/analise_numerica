20224ee70a9fcd1dd348fa3557133e18c6e44dcf
iamdiegosanches <dgsanches3@gmail.com>
2023-06-08 08:06:03 -0300

    atualizacao

--- Questionarios/graficos_para_diferentes_funcoes/principal.m
+++ Questionarios/graficos_para_diferentes_funcoes/principal.m
@@ -1,11 +1,11 @@
 clear
-% Definição dos valores de t
+# Definição dos valores de t
 t = linspace(2.55, 4.55, 4);
 
-% Definição dos valores de x
+# Definição dos valores de x
 x = linspace(-4.13, 12.39, 50);
 
-% Cálculo dos valores y para a curva atual
+# Cálculo dos valores y para a curva atual
 y = 9*sin(x.^1)*9.*cos(x.^10)*8*(t(1)^1)*8.*x.^10;
 plot(x, y, 'xm');
 hold on
