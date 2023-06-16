03f82f7f41309a398a4408d4dba3a8025f407ade
iamdiegosanches <dgsanches3@gmail.com>
2023-06-16 19:42:11 -0300

    update

--- Atividades/Apostila para as aulas praticas 1-5/pratica 1/A1_5.m
+++ Atividades/Apostila para as aulas praticas 1-5/pratica 1/A1_5.m
@@ -3,20 +3,28 @@ clc
 # a)
 
 format short
-res = (sqrt(5) + 3)/0.3541
+valor = (sqrt(5) + 3)/0.3541;
+fprintf("%.4f\n", valor);
+erro = (14.7870 - valor)/14.7870
 
 # b)
 
 format bank
-valor = (e^3+log(5))/(sin(3)+tan(0.5))
+valor = (e^3+log(5))/(sin(3)+tan(0.5));
+fprintf("%.3f\n", valor);
+erro = (31.560 - valor)/31.560
 
 # c)
 
 format bank
-res = log(5)/log(3)
+valor = log(5)/log(3);
+fprintf("%.2f\n", valor);
+erro = (1.46 - valor)/1.46
 
 # d)
 
 format short
-res = (3.16)^(1/3)
+valor = (3.16)^(1/3);
+fprintf("%.1f\n", valor);
+erro = (1.5 - valor)/1.5
 
