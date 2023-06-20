9c2e665f2f36f56d818dc87a8981f7a3de7c5e91
iamdiegosanches <dgsanches3@gmail.com>
2023-06-19 21:24:23 -0300

    update

--- Algoritmos/Testados/Cholesky/sol_Cholesky.m
+++ Algoritmos/Testados/Cholesky/sol_Cholesky.m
@@ -28,6 +28,15 @@ function x = sol_Cholesky (A, b)
     disp('nao e possivel resolver o sistema por Cholesky');
     return;
   endif
+  for i = 1 : size(A, 1)
+    for j = 1 : size(A, 1)
+      if A(i,j) ~= A(j, i)
+        disp('A matriz nao e simetrica');
+        return;;
+      endif
+    endfor
+  endfor
+
   L = tril(R);
   y = subst_sucess(L, b);
   x = subst_retro(L', y');
