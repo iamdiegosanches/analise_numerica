132cb75e46d56cef7f76e4ef43f8e01d721ec147
iamdiegosanches <dgsanches3@gmail.com>
2023-07-06 18:18:52 -0300

    revisao algoritmos splines

---
+++ Algoritmos/Testados/splines/splines_avaliar.m
@@ -0,0 +1,70 @@
+## Copyright (C) 2023 aluno
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn {} {@var{sz}, @var{Info} =} splines_avaliar (@var{n}, @var{x}, @var{y}, @var{m}, @var{z}, @var{ts})
+## entradas:
+## numero de pontos, vetor de abscissas em ordem crescente, vetor de ordenadas,
+## numero de pontos a interpolar, valores a interpolar e tipo de splines_avaliar
+## saidas
+## valores interpolados e informacao sobre erro, sendo Info = 0: sem erro, Info = -1:
+## numero de pontos de n<3 ou n <4 (depende do spline), Info = -2: abscissas nao estao
+## em ordem crescente e Info > 0: numero de tentativas de extrapolacao.
+## @seealso{}
+## @end deftypefn
+
+## Author: aluno <aluno@LI1-150814>
+## Created: 2023-06-27
+
+function [sz, Info] = splines_avaliar (n, x, y, m, z, ts)
+  if ts == 0
+    [s2, Info] = splines_naturais(n,x,y);
+  elseif
+    [s2, Info] = splines_extrapolados(n,x,y);
+  endif
+  if Info < 0
+    return;
+  endif
+  Info = 0;
+  for j = 1 : m
+    if (z(j) >= x(1)) && (z(j) <=  x(n))
+      # Pesquisa binarios para localizar o intervalo
+      inf = 1;
+      sup = n;
+      while 1
+        if (sup - inf) <= 1
+          break;
+        endif
+        ind  = round((inf+sup)/2);
+        if x(ind) > z(j)
+          sup = ind;
+        else
+          inf = ind;
+        endif
+      endwhile
+      # Avaliacao do spline pelo metodo de Horner
+      h = x(sup) - x(inf);
+      a = (s2(sup)-s2(inf))/(6*h);
+      b = s2(inf)/2;
+      c = (y(sup)-y(inf))/h-(s2(sup)+2*s2(inf))*h/6;
+      d = y(inf);
+      h = z(j)-x(inf);
+      sz(j)=((a*h+b)*h+c)*h+d;
+    else
+      sz(j) = 0;
+      Info = Info + 1;
+    endif
+  endfor
+endfunction
---
+++ Algoritmos/Testados/splines/splines_extrapolados.m
@@ -0,0 +1,95 @@
+## Copyright (C) 2023 Alvaro
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn {} {@var{s2}, @var{CondErro} =} splines_extrapolados (@var{n}, @var{x}, @var{y})
+## Entrada:
+## n: numero de pontos
+## x: vetor de abscissas em ordem crescente
+## y: vetor de ordenadas
+## Saida:
+## s2: derivadas segundas
+## Info: informacoes sobre erro:
+##        Info = 0: nao houve erro
+##        Info = -1: numero de pontos n<4
+##        Info = -2: abscissas nao estao em ordem crescente
+## @seealso{}
+## @end deftypefn
+
+## Author: Alvaro <Alvaro@DESKTOP-TG63L3N>
+## Created: 2023-07-06
+
+function [s2, Info] = splines_extrapolados (n, x, y)
+  if n < 4
+    Info = -1;
+    return;
+  endif
+  Ordenado = true;
+  for i = 2 : n
+    Ordenado = Ordenado & (x(i-1)<x(i));
+  endfor
+  if ~Ordenado
+    Info = -2;
+    return;
+  endif
+  Info = 0;
+  m = n-2;
+  # construcao so distema linear tridiagonal nao simetrico
+  Ha = x(2)-x(1);
+  Deltaa=(y(2)-y(1))/Ha;
+  Hb=x(3)-x(2);
+  Deltab=(y(3)-y(2))/Hb;
+  d(1)=(Ha+Hb)*(Ha+2*Hb)/Hb;
+  c(2)=Hb-(Ha^2)/Hb;
+  s2(2)=6*(Deltab-Deltaa);
+  for i = 2:m-1
+    ip1=i+1;
+    ip2=i+2;
+    Ha=Hb;
+    Deltaa=Deltab;
+    Hb=x(ip2)-x(ip1);
+    Deltab=(y(ip2)-y(ip1))/Hb;
+    d(i)=2*(Ha+Hb);
+    e(i-1)=Ha;
+    c(ip1)=Hb;
+    s2(ip1)=6*(Deltab-Deltaa);
+  endfor
+  Ha=Hb;
+  Deltaa=Deltab;
+  Hb=x(n)-x(n-1);
+  Deltab=(y(n)-y(n-1))/Hb;
+  d(m)=(Ha+Hb)*(Hb+2*Ha)/Ha;
+  e(m-1)=Ha-(Hb^2)/Ha;
+  s2(m+1)=6*(Deltab-Deltaa);
+  #eliminacao de Gauss
+  for i = 2:m
+    ip1=i+1;
+    im1=i-1;
+    t=e(im1)/d(im1);
+    d(i)=d(i)-t*c(i);
+    s2(ip1)=s2(ip1)-t*s2(i);
+  endfor
+  # solcao por substituicoes retroativas
+  s2(m+1)=s2(m+1)/d(m);
+  for i=m:-1:2
+    s2(i)=(s2(i)-c(i)*s2(i+1))/d(i-1);
+  endfor
+  Ha=x(2)-x(1);
+  Hb=x(3)-x(2);
+  s2(1)=((Ha+Hb)*s2(2)-Ha*s2(3))/Hb;
+  Ha=x(n-1)-x(n-2);
+  Hb=x(n)-x(n-1);
+  s2(n)=((Ha+Hb)*s2(n-1)-Hb*s2(n-2))/Ha;
+endfunction
